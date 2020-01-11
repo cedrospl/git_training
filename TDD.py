e3cdedff36f194660f6e88c5075e0373eec0ddd9
cedrospl <[hagnar93@gmail.com]>
2020-01-11 12:20:40 +0100

    updateUnitTests

---
+++ TDD.py
@@ -0,0 +1,4 @@
+def f1(x,y = 0):
+    return (x * x) + y
+
+
--- test_TDD.py
+++ test_TDD.py
@@ -1,11 +1,36 @@
 import unittest
+import TDD
 
 class testUnitTest(unittest.TestCase) :
 
     def test_g(self):
         self.assertTrue(True)
+    
+    def test_f1_1(self):
+        w1 = 0
+        w2 = TDD.f1(w1)
+        self.assertEqual(w1,w2)
+   
+    def test_f1_2(self):
+        w1 = 1
+        w2 = TDD.f1(w1)
+        self.assertEqual(w1,w2)
+    
+    def test_f1_3(self):
+        w1 = 4
+        w2 = TDD.f1(2)
+        self.assertEqual(w1,w2)
 
+    def test_f1_4(self):
+        w1 = 5
+        w2 = TDD.f1(2,1)
+        self.assertEqual(w1,w2)
 
+    def test_f1_5(self):
+        w1 = 7
+        w2 = TDD.f1(2,3)
+        self.assertEqual(w1,w2)
+        
 
 if __name__ == '__main__':
     unittest.main()
